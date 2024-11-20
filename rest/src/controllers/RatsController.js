d8eff89b4816b441ced962c34ceb8677b2c59a99
Michael Giesler <michael.giesler1@icloud.com>
2024-11-20 14:41:14 -0700

    set up of rats controller  -- particularly console log test

--- rest/src/controllers/RatsController.js
+++ rest/src/controllers/RatsController.js
@@ -3,4 +3,15 @@ import BaseController from "../utils/BaseController.js";
 
 
 
-export class RatsController extends BaseController
+export class RatsController extends BaseController {
+
+  constructor() {
+    super('api/rats')
+    this.router.get('', this.test)
+  }
+
+  test(request, response, next) {
+    console.log('Successful Test')
+    response.send("Successful Test!")
+  }
+}
