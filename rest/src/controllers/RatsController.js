0b914b7a424743f7af2cd528949281fb8eaa3c73
Michael Giesler <michael.giesler1@icloud.com>
2024-11-21 13:29:09 -0700

    added get request tasked with GETing all rats

--- rest/src/controllers/RatsController.js
+++ rest/src/controllers/RatsController.js
@@ -3,3 +3,11 @@ const router = express.Router();
 const Rat = require('../models/Rat');
 const Mission = require('../models/Mission');
 
+router.get('/', async (req, res) => {
+  try {
+    const rats = await Rat.find();
+    res.json(rats);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
