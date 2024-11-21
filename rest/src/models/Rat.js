6af1ef6667480848ae43deda96deb74bab925f3d
Michael Giesler <michael.giesler1@icloud.com>
2024-11-21 13:14:17 -0700

    added specialities schema to rats.js

--- rest/src/models/Rat.js
+++ rest/src/models/Rat.js
@@ -3,5 +3,8 @@ import { Schema } from "mongoose";
 export const RatSchema = new Schema({
   name: { type: String, required: true },
   picture: { type: String, required: true },
-  callsign: { type: String, required: true }
-})
+  callsign: { type: String, required: true },
+  specialities: { type: [String], required: true },
+});
+
+module.exports = mongoose.model('Rat', RatSchema);
