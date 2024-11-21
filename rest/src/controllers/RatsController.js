433ddf18cd916b493e1cd5845d15a6a2e6c8005c
Michael Giesler <michael.giesler1@icloud.com>
2024-11-21 13:34:01 -0700

    added funcationality for GETing missions assigned to a specific rat

--- rest/src/controllers/RatsController.js
+++ rest/src/controllers/RatsController.js
@@ -11,3 +11,15 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+router.get('/:id/missions', async (req, res) => {
+  try {
+    const missions = await Mission.find({ ratId: req.params.id })
+      .populate({ path: 'location', select: 'country zone -_id' })
+    res.json(missions);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
