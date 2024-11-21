7b980e4c95878f5bc2a977deece640619d676279
Michael Giesler <michael.giesler1@icloud.com>
2024-11-21 13:25:26 -0700

    added constants to import express & the rat/mission models

--- rest/src/controllers/RatsController.js
+++ rest/src/controllers/RatsController.js
@@ -1,51 +1,5 @@
-import BaseController from "../utils/BaseController.js";
+const express = require('express');
+const router = express.Router();
+const Rat = require('../models/Rat');
+const Mission = require('../models/Mission');
 
-
-
-
-export class RatsController extends BaseController {
-
-  constructor() {
-    super('api/rats')
-    this.router.get('', this.getRats)
-    this.router.get('/test', this.test)
-    this.router.get('', this.addRat)
-    this.router.get('/:ratId', this.getOneRat)
-  }
-
-  test(request, response, next) {
-    console.log('Successful Test')
-    response.send("Successful Test!")
-  }
-
-  async getRats(request, response, next) {
-    try {
-      const Rats = await ratsService.getRats()
-      response.send('rats')
-    } catch (error) {
-      next(error)
-    }
-  }
-
-  async getOneRat(request, reponse, next,) {
-    try {
-      const ratId = request.params.ratId
-      console.log('getting', ratId);
-      const rat = await atService.getOneRat
-      response.send('getting your rat')
-    } catch (error) {
-      next(error)
-    }
-  }
-
-
-  async addRat(request, response, next) {
-    try {
-      const ratData = request.body
-      const rat = ratsService.addRat(ratData)
-      response.send(rat)
-    } catch (error) {
-      next(error)
-    }
-  }
-}
